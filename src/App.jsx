5300028b1ee796245389cd5ec14c614495f980b9
cdsouza <cedricdsouza98@gmail.com>
2025-03-11 23:21:41 +0530

    first commit after vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-center bg-gray-900 text-sky-500 h-20 ">
+        Netflix GPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
